74580d85127ec865dfcdc3b9a48cb86eacf1b4b6
Farsos Bulsara <kingsukhoi@gmail.com>
2021-01-19 18:27:06 -0700

    Added testing files

---
+++ mountMe/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Container Test</title>
+</head>
+<body>
+<h1>Hello world</h1>
+<?php
+$hello="hello world";
+$num=5;
+echo "<p>test</p>";
+echo "<a href=./page2.php>Click Me</a>"
+?>
+</body>
+</html>
---
+++ mountMe/page2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Container Test</title>
+</head>
+<body>
+<h1>Page 2</h1>
+<?php
+$hello="hello world";
+$num=5;
+echo "<p>test</p>";
+?>
+</body>
+</html>
